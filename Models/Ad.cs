53928eb01c16bc471dc8ee3bd1a9bf21672edb7d
ElMarto12 <91432739+ElMarto12@users.noreply.github.com>
2024-03-21 21:23:10 +0200

    Updated "Ad" class

--- Models/Ad.cs
+++ Models/Ad.cs
@@ -7,15 +7,21 @@ public class Ad
     [Key]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
+    
+    [Required]
+    public string OwnerId { get; set; }
 
     [Required]
+    [Column(TypeName = "varchar(65)")]
     public string Title { get; set; }
 
     [Required]
     [Column(TypeName = "decimal(18, 2)")]
     public decimal Price { get; set; }
-
+    
+    [Column(TypeName = "varchar(255)")]
     public string Description { get; set; }
 
+    [Required]
     public string ImagePath { get; set; }
 }
